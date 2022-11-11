9364e2453f7c7dfb34e10187486691f89dca27e0
AbrahamI715 <90865860+AbrahamI715@users.noreply.github.com>
2022-11-11 11:48:14 +0000

    updated version
    newer game with semi working anims

---
+++ add_on.py
@@ -0,0 +1,18 @@
+from os import walk  # a useful library that we can access the os module functions
+import pygame.image
+import pygame
+
+
+def import_folder(path):
+    surface_list = []  # to get a list of strings (each file)
+
+    for _, __ , img_files in walk(path):  # walk uses 3 pieces of info
+        image_scale = 2.5
+        for image in img_files:
+            full_path = path + '/' + image  # so we have all code needed from code to img
+            img_surface = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(pygame.transform.rotozoom(img_surface, 0, image_scale))  # rescaling the img and loading
+
+    return surface_list
+
+# this function allows us to access any of the folders and import their images
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 import pygame
+from add_on import import_folder
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('#33adff')
+        self.character_assets()
+        self.frame_index = 0  # used to pick out one of the anim frames
+        self.anim_speed = 0.06
+        self.image = self.anims['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
 
         # player movement
@@ -15,7 +18,22 @@ class Player(pygame.sprite.Sprite):
         self.jump_speed = -16
 
     def character_assets(self):  # Allows us to access the path to character assets sheet
-        path_to_char_folder = 'Character_assets/character_sheet.png'
+        path_to_char_folder = 'Character_assets/'
+        self.anims = {'idle': [], 'jump': [], 'run': []}  # using a dictionary to easily access the folder we want
+
+        for animations in self.anims.keys():
+            complete_path = path_to_char_folder + animations    # the animations is one of the lists in the dictionary
+            # which we attach to the back of the file path
+            self.anims[animations] = import_folder(complete_path)  # to get the animation we want to work on
+
+    def animate(self):
+        current_anim = self.anims['idle']
+
+        self.frame_index += self.anim_speed
+        if self.frame_index >= len(current_anim):  # once frame index is greater than the number of items in the list
+            self.frame_index = 0
+
+        self.image = current_anim[int(self.frame_index)]
 
     def get_input(self):  # to get all the keys the player presses
         key_press = pygame.key.get_pressed()
@@ -39,5 +57,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):  # used to control sprite behaviour
         self.get_input()
+        self.animate()
         self.activate_gravity()
 
